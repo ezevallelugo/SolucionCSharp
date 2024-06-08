5c3d0d7cc166e8df64969871a24013797fcbfec9
Eze V <eze0072@hotmail.com>
2024-06-08 17:22:37 -0300

    El cambio del sabado

---
+++ Clase11/Biblioteca2/CompetenciaNoDisponibleException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca2
+{
+    public class CompetenciaNoDisponibleException : Exception
+    {
+        private string nombreClase;
+        private string nombreMetodo;
+        public CompetenciaNoDisponibleException(string mensaje, string metodo)
+        {
+
+        }
+        public CompetenciaNoDisponibleException(string mensaje, string metodo, Exception innerException)
+        {
+
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Excepcion del metodo {this.nombreMetodo} de la clase {this.nombreClase}");
+            stringBuilder.AppendLine($"Mensaje propio de la excepcion");
+            stringBuilder.AppendLine("Todos los inner exception");
+            return stringBuilder.ToString();
+        }
+    }
+}
